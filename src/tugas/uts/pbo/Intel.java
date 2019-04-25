f3656396f9a6e0f7de94d6f4fb7aadda03639560
Naufal Farras D. W <47814447+Naufal-FTech@users.noreply.github.com>
2019-04-25 22:43:04 +0700

    Update Intel.java

--- src/tugas/uts/pbo/Intel.java
+++ src/tugas/uts/pbo/Intel.java
@@ -5,7 +5,7 @@ package tugas.uts.pbo;
  * @author Phyton NF
  */
 public class Intel extends CPU {
-    public Intel(int kecepatan) {
+    public Intel(int kecepatan) {   //int kecepatan = 2 yg mana nilai 2 dibawakan oleh construktor Komputer dg parameter objek "intel1" pd class Intel
         super("Intel ", kecepatan);
     }    
 }
