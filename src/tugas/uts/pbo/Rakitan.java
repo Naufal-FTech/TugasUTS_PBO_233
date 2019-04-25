cbcbe7b1e2910e229e497612213eb570c799ced8
Naufal Farras D. W <47814447+Naufal-FTech@users.noreply.github.com>
2019-04-25 22:40:02 +0700

    Update and rename MainClass.java to Rakitan.java

--- src/tugas/uts/pbo/Rakitan.java
+++ src/tugas/uts/pbo/Rakitan.java
@@ -2,10 +2,10 @@ package tugas.uts.pbo;
 
 
 
-public class MainClass {
+public class Rakitan {
     public static void main(String[] args) {
-        Intel intel1 = new Intel(2);
-        AMD amd1 = new AMD(3);
+        Intel intel1 = new Intel(2);    //set nilai parameter = 2
+        AMD amd1 = new AMD(3);          //set nilai parameter = 3
         Komputer komputer1 = new Komputer(intel1);  //parameter constructor berisi objek "intel1" yang telah diset bernilai 2
         
         komputer1.cetakInfo();
